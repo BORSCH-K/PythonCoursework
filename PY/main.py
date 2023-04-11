f48d91190c8984dbf3de6842e8d8bd42175e4d04
Ксения <urukinaksusha@gmail.com>
2023-04-11 22:35:47 +0600

    (M)

--- PY/main.py
+++ PY/main.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 import timeit
 
 INITIAL_FREQ = 1000  # начальная частота
-LEN_RANGE = 1000  # длина диапазона
-N = 10000  # количество повторений fft
+LEN_RANGE = 201  # длина диапазона
+N = 1000 # количество повторений fft
 
 
 def time(i):
@@ -39,31 +39,44 @@ time_fft = []  # время на ДПФ
 locale_rate = 1000  # начальная частота, изменяемая
 k_points = []  # список точкей с наилучшей частотой
 
+X = []  # время для ? частоты * 10
+M = []  # среднее значение х
+D = []  # среднеквадратичное отклонение
 for range_element in range(INITIAL_FREQ, LEN_RANGE+INITIAL_FREQ):  # range_element - элемент диапазона
-    time_fft.append(time(range_element))
+    x = []
+    for i in range(0,10):
+        x.append(time(range_element))
+    X.append(x)
+    M.append(M_(x))
+    j = range_element - INITIAL_FREQ
+    time_fft.append(M[j])
+    D.append((D_(x, M[j])))
+    print(range_element, M[j], D[j], (D[j]/M[j]*100))
+    # print(x)
 
 k = INITIAL_FREQ  # текущая частота
-while k < (LEN_RANGE + INITIAL_FREQ):
+while k < (LEN_RANGE + INITIAL_FREQ): # [1000, 1024, 1080, 1125, 1152, 1200]
     k_temp = nfl(k)
     k_points.append(k_temp)
     k = k_temp + 1
 print(k_points)
 
-X = []  # время для k-ой частоты * 10
-M = []  # среднее значение х
-D = []  # среднеквадратичное отклонение
+k_time = []
 for i in range(0, len(k_points)):
-    x = []
-    print(k_points[i])
-    for j in range(0, 10):
-        x.append(time(k_points[i]))
-    X.append(x)
-    M.append(M_(x))
-    D.append((D_(x, M[i])))
-
-    print(i, M[i], D[i])
-    print(x)
-
+    k_time.append(time_fft[k_points[i]- INITIAL_FREQ])
+print(k_time)
+# for i in range(0, len(k_points)):
+#     x = []
+#     print(k_points[i])
+#     for j in range(0, 10):
+#         x.append(time(k_points[i]))
+#     X.append(x)
+#     M.append(M_(x))
+#     D.append((D_(x, M[i])))
+#
+#     print(i, M[i], D[i])
+#     print(x)
+print(time_fft) # средние значения
 plt.plot(range(INITIAL_FREQ, INITIAL_FREQ + LEN_RANGE), time_fft)
-plt.plot(k_points, M, 'o')
+plt.plot(k_points, k_time, 'o')
 plt.show()
